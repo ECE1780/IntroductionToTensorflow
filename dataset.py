dc69827396a27248a83d283947036a3a40aa2e2c
ECE1780 <p@arh.am>
2018-01-24 21:21:00 -0500

    Initial commit

---
+++ dataset.py
@@ -0,0 +1,17 @@
+import numpy as np
+from PIL import Image
+import random
+
+
+class MnistDataset:
+
+    def __init__(self, data_dir):
+        self.position = 0
+        pass
+
+    def sample(self, batch_size):
+        pass
+
+    @staticmethod
+    def read_image(self, image_path):
+        pass
